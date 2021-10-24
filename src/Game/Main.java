f421250f2ae7dce172a0c62ca00d57631561fa28
Rodya <89155887342@yandex.ru>
2021-10-24 21:17:40 +0300

    Re Main

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -3,10 +3,12 @@ package Game;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Game game = new Game();
+    public static void main(String[] args) {
         try {
+            Game game = new Game();
             game.gameLines();
+        } catch (IOException e) {
+            System.out.println(e);
         } catch (Exception e) {
             System.out.println(e);
         }
